b015fcd0a1a0cde9732ce7bbf17d9562c00e4c20
nicolly ketlyn <nicollyketlyn97@gmail.com>
2022-11-16 16:31:20 -0300

    exercicio condicionais

---
+++ src/Condicionais.java
@@ -0,0 +1,26 @@
+public class Condicionais {
+    public static void main(String[] args) {
+        /// Resultado dos alunos 1
+        double notasAlunos = (5.0 + 8.0 + 6.0 + 7.0) / 4.0;
+        double mediaFinal = notasAlunos;
+
+        System.out.println("A média final é " + mediaFinal);
+        if (mediaFinal < 6) {
+            System.out.println("Reprovado");
+        } else {
+            System.out.println("Aprovado");
+        }
+       /// Resultados alunos 2
+        double notasAlunos2 = (2.0 + 4.0 + 3.0 + 1.0) / 4.0;
+        double mediaFinal2 = notasAlunos2;
+
+        System.out.println("A média final é " + mediaFinal2);
+        if (mediaFinal2 < 6) {
+            System.out.println("Reprovado");
+        } else {
+            System.out.println("Aprovado");
+
+        }
+
+    }
+}
---
+++ src/Condicionais2.java
@@ -0,0 +1,21 @@
+public class Condicionais2 {
+    public static void main(String[] args) {
+
+        double grauC = 30.0;
+        String grausF = "C";
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
